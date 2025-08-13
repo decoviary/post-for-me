a5dc14f6ab5b568b4f8d300d78abb565287613fa
Matthew Rothenberger <matt@daymoon.io>
2025-08-13 10:34:29 -0400

    Update dashboard/app/routes/_auth.sign-in._index/_otp-form.tsx
    Co-authored-by: Caleb Panza <45076058+calebpanza@users.noreply.github.com>

--- dashboard/app/routes/_auth.sign-in._index/_otp-form.tsx
+++ dashboard/app/routes/_auth.sign-in._index/_otp-form.tsx
@@ -137,17 +137,6 @@ export function OtpForm() {
           Send Code
         </Button>
 
-        {/* <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
-          <span className="bg-background text-muted-foreground relative z-10 px-2">
-            or
-          </span>
-        </div>
-
-        <div className="grid gap-3">
-          <Button asChild variant="secondary" className="w-full">
-            <Link to="/sign-in/magic-link">Sign in with Magic Link</Link>
-          </Button>
-        </div> */}
       </div>
 
       <div className="text-center text-sm">
