74842c979467ad66b88c2f149dec8e7d1907d0e8
matt rothenberger <matt@daymoon.io>
2025-08-14 16:11:42 -0400

    check in

--- api/src/getting-started.md.ts
+++ api/src/getting-started.md.ts
@@ -1,11 +1,38 @@
 export const gettingStartedDescription = `
-To create a post you first need to get the social accounts you want to post to, upload any media for the post (if applicable), and then create the post. 
+The official API for [Post for Me](https://www.postforme.dev/)
 
-You only need to get the social accounts once as the ids will not change, however you will need to upload media each time you create a post.
+## Auth
+Authentication is required for all endpoints. Provide a valid API key as a Bearer token in the Authorization header. 
+Log in to your [Post for Me](https://www.postforme.dev/) account to retrieve your API key.
+
+## Getting Started
+To create a post you first need to get the social accounts you want to post to, upload any media for the post (if applicable), and then create the post. 
 
 The below example shows how to create a media post from start to finish.
 
 **Example flow using JavaScript and the Fetch API:**
+
+**Connect an Account**
+
+ You can skip this step if you have accounts connected already, or are connecting accounts through the [dashboard](https://app.postforme.dev/)
+\`\`\`js
+   //Create an auth URL to redirect the user to in order for them to login and connect their account
+   const socialAccountAuthUrlResponse = await fetch('https://api.postforme.dev/v1/social-accounts/auth-url', {
+     method: 'POST',
+     headers: {
+       'Content-Type': 'application/json',
+       'Authorization': 'Bearer YOUR_API_KEY'
+     },
+     body: JSON.stringify({
+         platform: 'facebook'
+       }),
+   });
+
+   const { url } = await socialAccountAuthUrlResponse.json();
+
+   //Redirect to the url from the repsone
+   console.log(url);
+   \`\`\`
 
 **Get Social Accounts**
 
@@ -57,5 +84,11 @@ The below example shows how to create a media post from start to finish.
    \`\`\`
 
 **Thats It! You have successfully scheduled a post to go out in one hour.** 
+
+## Client Libraries
+
+Follow the links below for our official libraries: 
+
+[TypeScript](https://www.npmjs.com/package/post-for-me)
 
 `;
